6fcc03e3a098b35d245ea5b70269d412f8811fa3
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-04 23:49:48 +0200

    Update app.py

--- UI/app.py
+++ UI/app.py
@@ -8,14 +8,36 @@ from sklearn.preprocessing import LabelEncoder
 #%%
 df = pd.read_csv(r'data/all_conversions_variables.csv')
 
-le = LabelEncoder()
+df = df[['num_sessions', 'city', 'country', 
+        'device_class', 'instant_booking', 
+        'user_verified', 'days'
+        ]]
 
+le = LabelEncoder()
+# %%
+df['city_encoded'] = le.fit_transform(df.city)
+df['country_encoded'] = le.fit_transform(df.country)
+df['device_class_encoded'] = le.fit_transform(df.device_class)
+df['instant_booking_encoded'] = le.fit_transform(df.instant_booking)
+df['user_verified_encoded'] = le.fit_transform(df.user_verified)
+
+#%% 
+#aindex = df[df['city']=='Kaiserslautern']['city_encoded']#.reset_index()#.reindex([0])
+avale = df[df['city']=='Kaiserslautern']['city_encoded'].item()#.unique().item(#[aindex]
+
+bvale = df[df['city']=='Kaiserslautern']['country_encoded'].item()#.unique()
+print([avale,bvale])
+#%%
+df[df['city']=='Kaiserslautern']['city_encoded'][11]
+
+
+#[avale, bvale]
 #%%
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     dbc.Label("Select characteristics of online visitor to predict the number of booking days"),
-    dbc.Row([dbc.Col([dcc.Dropdown(placeholder='Number of sessions by site visitor',
+    dbc.Row([dbc.Col(lg=4,children=[dcc.Dropdown(placeholder='Number of sessions by site visitor',
                                    options=[
                                        {'label': num_session, 'value': num_session}
                                        for num_session in range(1,11)
@@ -25,20 +47,49 @@ app.layout = html.Div([
                                    )
                       ]
                      ),
-            dbc.Col([dcc.Dropdown(
+            dbc.Col(lg=4,children=[dcc.Dropdown(
                                     placeholder='city of vistor',
-                                   options={'label': df['city'].unique(),
-                                             'value': le.fit_transform(df['city'])
-                                            }      
+                                   options=[{'label': city,
+                                             'value': city
+                                            }
+                                            for city in df['city'].unique()
+                                            ]      
                                    )
                       ]
                      ),
-            dbc.Col()
+            dbc.Col(lg=4,
+                    placeholder='Is the visitor verified on platform',
+                    children=[dcc.Dropdown(id='user_verified',
+                                                options=[{'label': user_verified, 'value': user_verified}
+                                                         for user_verified in df['user_verified'].unique()
+                                                         ]
+                                                )
+                                   ]
+                    )
             ]
             ),
     html.Br(), html.Br(),
     
-    dbc.Row([dbc.Col(), dbc.Col(), dbc.Col()
+    dbc.Row([dbc.Col(lg=4,
+                     children=[dcc.Dropdown(id='device',
+                                            placeholder='type of device used on platform',
+                                            options=[{'label': device_class, 'value': device_class}
+                                                     for device_class in df['device_class'].unique()
+                                                     ]
+                                            )
+                               ]
+                     ), 
+             dbc.Col(lg=4,
+                     children=[dcc.Dropdown(
+                                            children=[dcc.Dropdown(id='instant_book',
+                                                                   options=[{'label': instant_booking, 'value': instant_booking}
+                                                                            for instant_booking in df['instant_booking'].unique()
+                                                                            ]
+                                                                   )
+                                                      ]
+                                            )
+                               ]
+                     ), dbc.Col()
              ]
             ),
     dbc.Row([dbc.Button(),
