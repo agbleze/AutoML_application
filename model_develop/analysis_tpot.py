e6f6ebd14ba836814fe70c7bb331f9bd03ec4e09
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-15 21:47:15 +0200

    Update analysis_tpot.py

--- model_develop/analysis_tpot.py
+++ model_develop/analysis_tpot.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import r2_score,mean_squared_error
 import numpy as np
+import joblib
 
 #%% import data set
 df = pd.read_csv(r'data/all_conversions_variables.csv')
@@ -53,13 +54,14 @@ optimizer_pipeline.fit(features=X_train, target=y_train)
 
 # %%
 optimizer_pipeline.score(X_test, y_test)
-# %%
 
+# %%
 fitted_pipeline = optimizer_pipeline.fitted_pipeline_
 # %%
 fitted_pipeline
 # %%
 type(fitted_pipeline)
+
 # %%
 evaluated_models = optimizer_pipeline.evaluated_individuals_
 # %%
@@ -68,40 +70,48 @@ optimizer_pipeline.predict(X_test)
 y_test
 # %% check score on training dataset -- No overfitting because it test error is lower
 optimizer_pipeline.score(X_train, y_train)
+
 # %%
-optimizer_pipeline.export('/Users/lin/Documents/python_venvs/tpot_homelike_env/model_develop/optimized_model.py')
+optimizer_pipeline.export('/Users/lin/Documents/python_venvs/tpot_homelike_env/machine_learning_api/model_develop/optimized_model.py')
 
 #%%
 #optimizer_pipeline.export('/Users/lin/Documents/python_venvs/tpot_homelike_env/model_develop/gene100_model.py')
 # %%
 rmse = lambda y, y_hat: np.sqrt(mean_squared_error(y, y_hat))
+
 # %%
 y_pred = optimizer_pipeline.predict(X_test)
+
 # %%
 y_pred
 
 # %% only 27% of the data is explained by the predictors
 r2_score(y_test, y_pred)
+
 # %%
 mean_squared_error(y_test, y_pred)
 # %% the model makes a prediction with an average error of 88 days
 rmse(y_test, y_pred)
-# %%
 
+# %%
 df.user_verified_encoded.value_counts()
 df.instant_booking_encoded.value_counts()
-# %%
-import joblib
+
+#%%
+X_train.shape
 
 # %%
-joblib.dump(value=fitted_pipeline, filename='gene100_model.model')
+joblib.dump(value=fitted_pipeline, filename='new_model.model')
 # %%
-loaded_model = joblib.load(filename='booking_model.model')
+loaded_model = joblib.load(filename='new_model.model')
+
+#%%
+
 # %%
 len(X_train.columns)
 X_train.columns
 # %% make prediction with model
-loaded_model.predict([[2, 4, 2, 9, 3, 7, 8]])
+loaded_model.predict([[2, 4, 2, 9, 3, 7]])
 
 
 
