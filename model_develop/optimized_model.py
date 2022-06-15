f4dbe36369b3ae3291f512242e2af3b7238951c5
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-15 21:47:21 +0200

    Update optimized_model.py

--- model_develop/optimized_model.py
+++ model_develop/optimized_model.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor
-from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
-from tpot.builtins import StackingEstimator
+from xgboost import XGBRegressor
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,12 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=0)
 
-# Average CV score on the training set was: -13516.451398315821
+# Average CV score on the training set was: -11567.46253807556
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=49),
     PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=AdaBoostRegressor(learning_rate=1.0, loss="linear", n_estimators=100)),
-    GradientBoostingRegressor(alpha=0.85, learning_rate=0.5, loss="lad", max_depth=1, max_features=0.9000000000000001, min_samples_leaf=8, min_samples_split=12, n_estimators=100, subsample=0.55)
+    XGBRegressor(learning_rate=0.1, max_depth=9, min_child_weight=14, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.9000000000000001, verbosity=0)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 0)
