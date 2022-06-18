d1cc9daa5476d1cd4a1e344919386e58d7c4533b
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-18 02:37:46 +0200

    Update ui_helper.py

--- UI/ui_helper.py
+++ UI/ui_helper.py
@@ -1,10 +1,38 @@
 #%%
 import requests
 import json
+from typing import List
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def request_prediction(URL: str, data: dict) -> int:
     req = requests.post(url=URL, json=data)
     response = req.content
     prediction = json.loads(response)['predicted_value'][0]
     return prediction
+
+
 # %%
+def create_encoded_data(data: pd.DataFrame, columns: List = None):
+    le = LabelEncoder()
+    if columns == None:
+        columns = data.columns
+        for column in columns:
+            data[f'{column}_encoded'] = le.fit_transform(data[column])
+        return data
+    else:
+        if isinstance(columns, str):
+            columns = [columns]
+            for column in columns:
+                data[f'{column}_encoded'] = le.fit_transform(data[column])
+            return data
+        else:
+            for column in columns:
+                data[f'{column}_encoded'] = le.fit_transform(data[column])
+            return data
+
+
+
+
+
+
