49bbb1429ad2ed72559567cf78e9349fba2cdef8
Agbleze <lagbleze@st.ug.edu.gh>
2023-03-04 00:07:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(name='booking_gauger_tpoter',
+      version='0.0.1',
+      author='Agbleze Linus',
+      packages=['booking_gauger_tpoter'],
+      description='This package trains and develop machine learning model \
+                  for predicting number of days a room will be booked for accommodation \
+                 by an online visitor. The package uses AutoML under the hood to train \
+                and select best algorithm for the prediction.',
+    zip_safe=False,
+    license='MIT'
+      )
