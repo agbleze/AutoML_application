b8db28157f25bd2da7acde9884adeeb1f8589f36
Agbleze <lagbleze@st.ug.edu.gh>
2023-03-03 13:45:11 +0100

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -5,5 +5,11 @@ from argparse import Namespace
 args = Namespace(categorical_features = ['device_class', 'city', 'country', 
                                          'instant_booking', 'user_verified'
                                         ],
-                numeric_features = ['num_sessions']
+                numeric_features = ['num_sessions'],
+                features = ['num_sessions', 'device_class', 'city', 'country', 
+                                         'instant_booking', 'user_verified'
+                                        ],
+                target_variable = 'days',
+                data_foldername = 'data',
+                data_filename = 'all_conversions_variables.csv'
                 )
